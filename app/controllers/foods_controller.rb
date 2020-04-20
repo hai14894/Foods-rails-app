1e8df501dec0e100455eb5bccaf71e77d5f62f08
Hai <hohoanghai14894@gmail.com>
2020-04-20 22:33:49 +1000

    MVC added

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,4 +3,30 @@ class FoodsController < ApplicationController
     def index
         @foods = Food.all()
     end
+
+    def show
+        @showfood = Food.find(params["id"])
+    end
+
+    def new #cant use numbers with 0s in it
+
+    end
+
+    def create 
+        new_food = Food.create(name: params["name"], price: params["price"].to_i)
+        redirect_to index_path
+    end
+
+    def edit 
+        @food = Food.find(params["id"])
+    end
+    def update 
+        updated_food = Food.update(params["id"], name: params["name"], price: params["price"])
+        redirect_to show_path(params["id"])
+    end
+
+    def destroy 
+        destroyed_food = Food.find(params["id"]).destroy
+        redirect_to index_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
- get "/foods", to: "foods#index"
- get "/foods/:id", to: "foods#show"
+ get "/foods", to: "foods#index", as: "index"
+ get "foods/new", to: "foods#new", as: "new"
+ get "foods/:id/edit", to: "foods#edit", as: "edit"
+ get "/foods/:id", to: "foods#show", as: "show"
+ post "/foods", to: "foods#create"
+ put "/foods/:id", to: "foods#update"
+ delete "foods/:id", to: "foods#delete"
+
 
 end
